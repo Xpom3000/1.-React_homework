107d98dab64c70e97293b8f6979f6760d0dc77d8
Igor Shipitko <garikrhip20@gmail.com>
2024-02-01 20:27:33 +0300

    рц2 готова

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,66 @@
-import './App.css';
-import Column from './components/Column/Column';
-import Header from './components/Header/Header';
-import MainContent from './components/MainContent/MainContent';
-import PopBrowse from './components/popups/PopBrowse/PopBrowse';
-import PopExit from './components/popups/PopExit/PopExit';
-import PopNewCard from './components/popups/PopNewCard/PopNewCard';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Column from "./components/Column/Column";
+import Header from "./components/Header/Header";
+import MainContent from "./components/MainContent/MainContent";
+import PopBrowse from "./components/popups/PopBrowse/PopBrowse";
+import PopExit from "./components/popups/PopExit/PopExit";
+import PopNewCard from "./components/popups/PopNewCard/PopNewCard";
+import { cardList } from "./data";
+
+const statusList = [
+  "Без статуса",
+  "Нужно сделать",
+  "В работе",
+  "Тестирование",
+  "Готово",
+];
 
 function App() {
+  const [cards, setCards] = useState(cardList);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  function addCard() {
+    const newCard = {
+      id: cards.length + 1,
+      theme: "Web Design",
+      title: "Название задачи",
+      date: "30.10.23",
+      status: "Готово",
+    };
+    setCards([...cards, newCard]);
+  }
   return (
     <>
       <div className="wrapper">
         {/* pop-up start*/}
-        <PopExit/>
-        <PopNewCard/>
-        <PopBrowse/>
+        <PopExit />
+        <PopNewCard />
+        <PopBrowse />
         {/* pop-up end*/}
-            
-        <Header />
-        <MainContent>
-          <Column title={"Без статуса"} />
-          <Column title={"Нужно сделать"} />
-          <Column title={"В работе"} />
-          <Column title={"Теститрование"} />
-          <Column title={ "Готово"} />
-        </MainContent >
-          
+
+        <Header addCard={addCard} />
+        {isLoading ? (
+          "Загрузка..."
+        ) : (
+          <MainContent>
+            {statusList.map((status) => (
+              <Column
+                title={status}
+                key={status}
+                cardList={cards.filter((card) => card.status === status)}
+              />
+            ))}
+          </MainContent>
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,16 +1,17 @@
 import Card from "./CardsItem";
 
-export default function Column({title}) {
+export default function Column({title, cardList}) {
     return (
         <div className="main__column column">
             <div className="column__title">
               <p>{title}</p>
             </div>
-            <div className="cards">
-                <Card topic={"Research"} title={"Новая задача"} />
+        <div className="cards">
+          {cardList.map((card) => <Card topic={card.theme} title={card.title} key={card.id } /> )}
+                {/* <Card topic={"Research"} title={"Новая задача"} />
                 <Card topic={"Web design"} title={"Новая задача"} />
                 <Card topic={"Copywriting"} title={"Новая задача"} />
-                <Card topic={"Web design"} title={"Новая задача"} />
+                <Card topic={"Web design"} title={"Новая задача"} /> */}
             </div>
           </div>
     )
