1f931870788f37bdd58457aa685010a44913fcce
Igor Shipitko <garikrhip20@gmail.com>
2024-03-03 03:35:59 +0300

    hw3 готов здать на проверку

--- src/components/Column/Column.styled.js
+++ src/components/Column/Column.styled.js
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
+export const MainColumn = styled.div`
+  width: 20%;
+  margin: 0 auto;
+  display: block;
+`;
+
 export const ColumnTitle = styled.div`
   padding: 0 10px;
   margin: 15px 0;
